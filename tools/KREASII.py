14872ca0c18f99528805856d8f2fc9e299781f90
UrFutureDataAnalyst <ancamangagak@gmail.com>
2025-02-03 19:33:18 +0700

    coba

--- tools/KREASII.py
+++ tools/KREASII.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.markdown(
     """
     <div style='text-align: center;'>
-    <h1 style='font-size: 3em;'>Coming Soon!!!</h1>
+    <h1 style='font-size: 3em;'>Coming Soon!!!ppp</h1>
     </div>
     """,
     unsafe_allow_html=True,
